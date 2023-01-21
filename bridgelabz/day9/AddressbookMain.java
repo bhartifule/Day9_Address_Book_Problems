9c43db314fe7af98b5ce848141daa084eb90f059
Bharti <bharatifule10gmail.com>
2023-01-21 13:28:20 +0530

    Added:Adress Book Problems

--- bridgelabz/day9/AddressbookMain.java
+++ bridgelabz/day9/AddressbookMain.java
@@ -2,6 +2,24 @@ package com.bridgelabz.day9;
 
 public class AddressbookMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome in Address Book Problem");
-	}
+        System.out.println("Welcome to Address Book Program");
+        AddressBook obj1 = new AddressBook();
+        obj1.setName("Bharti");
+        obj1.setSurname("Fule");
+        obj1.setAddress("Hingna Nagpur");
+        obj1.setCity("Nagpur");
+        obj1.setState("Maharastra");
+        obj1.setContactNumber("9146075923");
+        obj1.setEmail("bharatifule10@gmail.com");
+
+        System.out.println("Name: " + obj1.getName());
+        System.out.println("Surname: " + obj1.getSurname());
+        System.out.println("Address: " + obj1.getAddress());
+        System.out.println("City: " + obj1.getCity());
+        System.out.println("State: " + obj1.getState());
+        System.out.println("ContactNumber: " + obj1.getContactNumber());
+        System.out.println("Email: " + obj1.getEmail());
+
+
+    }
 }
