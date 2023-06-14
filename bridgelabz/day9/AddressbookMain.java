9e2d4e71be2a680822b19529a0f2c3a4fd0167d8
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-14 22:02:09 +0530

    AddressbookMain.java

--- bridgelabz/day9/AddressbookMain.java
+++ bridgelabz/day9/AddressbookMain.java
@@ -3,7 +3,8 @@ package com.bridgelabz.day9;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AddressbookMain {
+public class AddressbookMain
+{
 	 public static void main(String[] args) {
 	        AddressBook familyBook = new AddressBook();
 	        System.out.println("Welcome in AddressBook system");
