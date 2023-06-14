2fcc1e959226b74208c0505b06e7864d6f7e861c
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-14 21:58:43 +0530

    AddressbookMain.java

--- bridgelabz/day9/AddressbookMain.java
+++ bridgelabz/day9/AddressbookMain.java
@@ -3,7 +3,8 @@ package com.bridgelabz.day9;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AddressbookMain {
+public class AddressbookMain
+{
 	public static void main1(String[] args) {}
 
 	public static void main(String[] args) {
